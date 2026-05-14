184c4216c049b6cb66626d05d793b19cc68569fe
Contractor-x <dada4ash@gmail.com>
2026-05-14 01:11:02 +0100

    added vite.config.ts

--- user/vite.config.ts
+++ user/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     return {
       base: './',
       server: {
-        port: 3000,
+        port: 8080,
         host: '127.0.0.1',
         strictPort: true,
       },
